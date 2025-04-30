7a6f2bee79cff33d5fe1af3d97f22335a6376c75
felix11 <khushkothari06@gmail.com>
2025-05-01 01:46:37 +0400

    alphafi-oracle package upgrade

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,12 @@ export * from "./core/client.js";
 export * from "./utils/oracle.js";
 export * from "./utils/priceFeedIds.js";
 export * from "./coin/index.js";
+export * from "./core/types.js";
 export { updatePythIdentifierForCoin } from "./admin/oracle.js";
 export { getUserPositionCapId } from "./models/position/functions.js";
 
 // Re-export key types for easier access
 export { AlphalendClient } from "./core/client.js";
-
-export {
-  SupplyParams,
-  WithdrawParams,
-  BorrowParams,
-  RepayParams,
-  ClaimRewardsParams,
-  LiquidateParams,
-  MAX_U64,
-  Market,
-  Position,
-  Portfolio,
-  Loan,
-  TransactionResponse,
-} from "./core/types.js";
 
 export function getSuiClient(network?: string) {
   const mainnetUrls = [
