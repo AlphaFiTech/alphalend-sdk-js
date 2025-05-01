1fb806deeb700a2c1480ed5ff86ed14b87f63188
felix11 <khushkothari06@gmail.com>
2025-05-01 09:37:52 +0400

    triton rpc url

--- src/index.ts
+++ src/index.ts
@@ -14,27 +14,7 @@ export { getUserPositionCapId } from "./models/position/functions.js";
 export { AlphalendClient } from "./core/client.js";
 
 export function getSuiClient(network?: string) {
-  const mainnetUrls = [
-    "https://fullnode.mainnet.sui.io/",
-    "https://mainnet.suiet.app",
-    "https://rpc-mainnet.suiscan.xyz/",
-  ];
-  const testnetUrls = ["https://fullnode.testnet.sui.io/"];
-  const devnetUrls = ["https://fullnode.devnet.sui.io/"];
-  const localnetUrls = ["http://localhost:9000"];
-
-  let urls: string[] = [];
-  if (network === "mainnet") {
-    urls = mainnetUrls;
-  } else if (network === "testnet") {
-    urls = testnetUrls;
-  } else if (network === "devnet") {
-    urls = devnetUrls;
-  } else {
-    urls = localnetUrls;
-  }
-
   return new SuiClient({
-    url: urls[Math.floor(Math.random() * urls.length)],
+    url: "https://alphalen-suimain-ef6f.mainnet.sui.rpcpool.com",
   });
 }
