5a1ef0089ea26f3e97a72a318c5690d918c2faf4
felix11 <khushkothari06@gmail.com>
2025-05-09 21:58:29 +0400

    exporting constants type

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,8 @@ import { alphafiConstants } from "./alphafiConstants.js";
 import { devConstants } from "./devConstants.js";
 import { prodConstants } from "./prodConstants.js";
 
+export { Constants } from "./types.js";
+
 export const getConstants = (network: string) => {
   if (network === "mainnet") return prodConstants;
   else return devConstants;
